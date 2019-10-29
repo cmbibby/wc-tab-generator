855892fca1a74fe7dbc7502ee393950de2e5706b
Chris Bibby <cmbibby@gmail.com>
2019-10-29 17:09:46 +0800

    include other one

--- class-wc-tab-generator.php
+++ class-wc-tab-generator.php
@@ -1,12 +1,14 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
+include_once(plugin_dir_path(__FILE__) . 'class-wc-tab-generator-tab.php');
+class WC_Tab_Generator
+{
 
-class WC_Tab_Generator {
 
-
-	public static function CreateTab( $args ) {
-		return new WC_Tab_Generator_Tab( $args );
+	public static function CreateTab($args)
+	{
+		return new WC_Tab_Generator_Tab($args);
 	}
 }
